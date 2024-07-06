9f2c5c7c4007f2110d4ab0b94039cb2454f067fa
jy-hwang <stratojy@gmail.com>
2024-07-06 18:53:40 +0900

    프로젝트 생성 후 첫 커밋
    프로젝트 생성 후 첫 커밋

---
+++ src/main/java/org/example/studyshop/StudyShopApplication.java
@@ -0,0 +1,13 @@
+package org.example.studyshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyShopApplication.class, args);
+    }
+
+}
