173507baefc241150b3928d81ec8cdfd14934b50
jy-hwang <stratojy@gmail.com>
2024-07-11 19:48:42 +0900

    Entity 생성과 Repository
    1. User Entity 를 생성.
    2. DB에 만들어놓은 Table과 똑같이 작성
    3. 몇가지 어노테이션을 붙여줌.
    4. JpaRepository 를 상속하는 UserRepository를 만들어줌.

---
+++ src/main/java/org/example/studyshop/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package org.example.studyshop.repository;
+
+import org.example.studyshop.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+}
