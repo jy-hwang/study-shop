b4a6365a7acf44293623200bcd3bfdba5356e63f
jy-hwang <stratojy@gmail.com>
2025-05-28 17:58:05 +0900

    SpringBoot 버전 변경
    1. 3.3.1 -> 2.7.18
    2. jakarta -> javax

--- src/main/java/org/example/studyshop/model/entity/Item.java
+++ src/main/java/org/example/studyshop/model/entity/Item.java
@@ -1,6 +1,6 @@
 package org.example.studyshop.model.entity;
 
-import jakarta.persistence.*;
+import javax.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
