c1f6f784673dc80f7c5fecc2dd817c2acacdef8d
jy-hwang <stratojy@gmail.com>
2025-05-29 09:22:54 +0900

    Repository Test 2
    1. User - create & read

--- src/test/java/org/example/studyshop/repository/UserRepositoryTests.java
+++ src/test/java/org/example/studyshop/repository/UserRepositoryTests.java
@@ -1,8 +1,6 @@
 package org.example.studyshop.repository;
 
-import jakarta.transaction.Transactional;
 import org.example.studyshop.StudyShopApplicationTests;
-import org.example.studyshop.model.entity.Item;
 import org.example.studyshop.model.entity.User;
 import org.example.studyshop.util.LoggingTestWatcher;
 import org.junit.jupiter.api.Assertions;
@@ -12,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
@@ -24,63 +23,53 @@ public class UserRepositoryTests extends StudyShopApplicationTests {
   @Test
   @DisplayName("User Repository Test - create")
   public void create() {
-    //String sql = insert into user (%s,%s,%d) values(account, email, age);
+    String account = "Test01";
+    String password = "11";
+    String status = "REGISTERED";
+    String email = "Test01@abc.com";
+    String phoneNumber = "010-1234-5678";
+    LocalDateTime registeredAt = LocalDateTime.now();
+    LocalDateTime createdAt = LocalDateTime.now();
+    String createdBy = "AdminServer";
+
     User user = User.builder()
-        .account("TestUser03")
-        .email("TestUser03@gmail.com")
-        .phoneNumber("010-1111-2233")
-        .createdAt(LocalDateTime.now())
-        .createdBy("ADMIN")
+        .account(account)
+        .password(password)
+        .status(status)
+        .email(email)
+        .phoneNumber(phoneNumber)
+        .registeredAt(registeredAt)
+        .createdAt(createdAt)
+        .createdBy(createdBy)
         .build();
 
     User newUser = userRepository.save(user);
-    System.out.println("newUser : " + newUser);
+    Assertions.assertNotNull(newUser);
+    Assertions.assertEquals(account, newUser.getAccount());
+    Assertions.assertEquals(status, newUser.getStatus());
   }
 
   @Test
   @DisplayName("User Repository Test - read")
   @Transactional
   public void read() {
-    Optional<User> user = userRepository.findByAccount("TestUser03");
+    User user = userRepository.findFirstByPhoneNumberOrderByIdDesc("010-1234-5678");
 
-    user.ifPresent(selectUser -> {
-      selectUser.getOrderDetails().forEach(detail -> {
-        Item item = detail.getItem();
-        System.out.println("item : " + item);
-      });
-    });
+    Assertions.assertNotNull(user);
   }
 
+  @Disabled
   @Test
   @DisplayName("User Repository Test - update")
   public void update() {
-    Long id = 2L;
-    Optional<User> user = userRepository.findById(id);
-    user.ifPresentOrElse(selectUser -> {
-          selectUser.setAccount("PPPPP");
-          selectUser.setUpdatedAt(LocalDateTime.now());
-          selectUser.setUpdatedBy("ADMIN");
-          userRepository.save(selectUser);
-          System.out.println("user : " + selectUser);
-        },
-        () -> System.out.println("The id you entered '" + id + "' is not exist user"));
+
   }
 
+  @Disabled
   @Test
-  @Transactional
   @DisplayName("User Repository Test - delete")
   public void delete() {
-    Long id = 1L;
-    Optional<User> user = userRepository.findById(id);
 
-    //Assert.assertTrue(user.isPresent()); junit4
-    Assertions.assertTrue(user.isPresent()); // junit5
-
-    user.ifPresent(selectUser -> userRepository.delete(selectUser));
-
-    Optional<User> deletedUser = userRepository.findById(id);
-
-    Assertions.assertFalse(deletedUser.isPresent());
   }
 
 }
