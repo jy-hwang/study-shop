f89dd4854c2b675eb53ba49d0574c863e7e3212d
jy-hwang <stratojy@gmail.com>
2025-05-30 10:13:57 +0900

    Repository Test 6
    1. OrderGroup

--- src/main/java/org/example/studyshop/model/entity/OrderGroup.java
+++ src/main/java/org/example/studyshop/model/entity/OrderGroup.java
@@ -48,4 +48,6 @@ public class OrderGroup {
   private LocalDateTime updatedAt;
 
   private String updatedBy;
+
+  private Long userId;
 }
---
+++ src/test/java/org/example/studyshop/repository/OrderGroupRepositoryTests.java
@@ -0,0 +1,41 @@
+package org.example.studyshop.repository;
+
+import org.example.studyshop.StudyShopApplicationTests;
+import org.example.studyshop.model.entity.OrderGroup;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class OrderGroupRepositoryTests extends StudyShopApplicationTests {
+
+  @Autowired
+  private OrderGroupRepository orderGroupRepository;
+
+  @Test
+  public void create() {
+
+    OrderGroup orderGroup
+        = OrderGroup.builder()
+        .status("COMPLETE")
+        .orderType("ALL")
+        .revAddress("서울시 강남구")
+        .revName("홍길동")
+        .paymentType("CARD")
+        .totalPrice(BigDecimal.valueOf(900000))
+        .totalQuantity(1)
+        .orderAt(LocalDateTime.now().minusDays(2))
+        .arrivalDate(LocalDateTime.now())
+        .createdAt(LocalDateTime.now())
+        .createdBy("AdminServer")
+        .userId(1L)
+        .build();
+
+    OrderGroup newOrderGroup = orderGroupRepository.save(orderGroup);
+
+    Assertions.assertNotNull(newOrderGroup);
+
+  }
+}
