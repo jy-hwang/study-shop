b2e1a70b61c436935d707f6809b816bd6980a21d
jy-hwang <stratojy@gmail.com>
2025-05-30 17:19:18 +0900

    프로젝트 개발 시작 전 마지막 준비
    1. CRUD 인터페이스 정의
    2. 요청/응답에 공통으로 사용할 Header 작성
    3. ResponseBody 공통부 작성

--- src/main/java/org/example/studyshop/controller/GetController.java
+++ src/main/java/org/example/studyshop/controller/GetController.java
@@ -1,6 +1,7 @@
 package org.example.studyshop.controller;
 
 import org.example.studyshop.model.SearchParam;
+import org.example.studyshop.model.network.Header;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,6 +30,12 @@ public class GetController {
     System.out.println(searchParam.getEmail());
     System.out.println(searchParam.getPage());
     return searchParam;
+  }
+
+  @GetMapping("/header")
+  public Header getHeader() {
+    // {"resultCode" : "OK","description" : "OK"}
+    return Header.builder().resultCode("OK").description("OK").build();
   }
 
 }
