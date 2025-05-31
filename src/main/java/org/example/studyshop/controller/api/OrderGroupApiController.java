28c893c130bbb747813ba57259cdfbeeb6995c53
jyhwang <stratojy@gmail.com>
2025-05-31 20:47:45 +0900

    #6 - feat. 주문내역 API Create
    1. Entity 속성 수정 int -> Long
    2. Controller -> Service

---
+++ src/main/java/org/example/studyshop/controller/api/OrderGroupApiController.java
@@ -0,0 +1,44 @@
+package org.example.studyshop.controller.api;
+
+import lombok.extern.slf4j.Slf4j;
+import org.example.studyshop.ifs.CrudInterface;
+import org.example.studyshop.model.network.Header;
+import org.example.studyshop.model.network.request.OrderGroupApiRequest;
+import org.example.studyshop.model.network.response.OrderGroupApiResponse;
+import org.example.studyshop.service.OrderGroupApiLogicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/orderGroup")
+public class OrderGroupApiController implements CrudInterface<OrderGroupApiRequest, OrderGroupApiResponse> {
+
+  @Autowired
+  private OrderGroupApiLogicService orderGroupApiLogicService;
+
+  @Override
+  @PostMapping("")
+  public Header<OrderGroupApiResponse> create(@RequestBody Header<OrderGroupApiRequest> request) {
+    log.info("Create OrderGroupApi requst : {}", request);
+    return orderGroupApiLogicService.create(request);
+  }
+
+  @Override
+  @GetMapping("{id}")
+  public Header<OrderGroupApiResponse> read(@PathVariable Long id) {
+    return null;
+  }
+
+  @Override
+  @PutMapping("")
+  public Header<OrderGroupApiResponse> update(@RequestBody Header<OrderGroupApiRequest> request) {
+    return null;
+  }
+
+  @Override
+  @DeleteMapping("{id}")
+  public Header delete(Long id) {
+    return null;
+  }
+}
